1ac3d4406a6e0c535126d5859bd1ed8d6b501f51
Muthoni Njoki <fmuthoninjoki@gmail.com>
2023-10-14 21:21:34 +0300

    Endpoint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
     const list = map;
 
-    if (list instaceof Map) {
+    if (list instanceof Map) {
         for (const [key, value] of list) {
             if (value === 1) {
                 list.set(key, 100);
